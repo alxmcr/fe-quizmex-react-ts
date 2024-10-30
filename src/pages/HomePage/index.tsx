59171f0c314bffac7a49b39143cd590851f56fdb
Alejandro Coca <alxmcr@outlook.com>
2024-10-30 02:34:59 -0400

    wip

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -3,7 +3,11 @@ import "./HomePage.scss";
 export function HomePage() {
   return (
     <main className="home-page">
-      <div className="home-page__container"></div>
+      <div className="home-page__container">
+        <button type="button" className="home-page__button">
+          Play
+        </button>
+      </div>
     </main>
   );
 }

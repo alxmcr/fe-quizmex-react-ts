8a38adda7da9ac306fa48ed94cb4c0c51e9fe5ce
Alejandro Coca <alxmcr@outlook.com>
2024-10-30 18:09:20 -0400

    wip

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,14 +1,17 @@
 import { AppButton } from "../../components/buttons/AppButton";
+import { Icon110x110Poncho } from "../../components/icons/110x110/Icon110x110Poncho";
 import "./HomePage.scss";
 
 export function HomePage() {
   return (
     <main className="home-page">
       <div className="home-page__container">
+        <Icon110x110Poncho />
+        <h1 className="home-page__title">QuizMex</h1>
+        <p className="home-page__description">
+          Play to test your knowledge about Mexico's history
+        </p>
         <AppButton>Play</AppButton>
-        <AppButton variant="perfect">Play Again</AppButton>
-        <AppButton variant="great-work">Play Again</AppButton>
-        <AppButton variant="failed">Try Again</AppButton>
       </div>
     </main>
   );

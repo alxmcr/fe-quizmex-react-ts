8e1048be345d7b01f081b0bba0b871029613a3a1
Alejandro Coca <alxmcr@outlook.com>
2024-10-30 19:28:21 -0400

    wip

--- src/pages/QuestionsPage/index.tsx
+++ src/pages/QuestionsPage/index.tsx
@@ -2,8 +2,15 @@ import "./QuestionsPage.scss";
 
 export function QuestionsPage() {
   return (
-    <main>
-      <h1>Questions Page</h1>
+    <main className="questions-page">
+      <div className="questions-page__container">
+        <div className="questions-page__details">
+          <h1 className="questions-page__title">QuizMex</h1>
+          <p className="questions-page__description">
+            Play to test your knowledge about Mexico's history
+          </p>
+        </div>
+      </div>
     </main>
   );
 }

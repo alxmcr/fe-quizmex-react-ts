d38a9e8b5a0f60da7d5760e7c83b387a063791f7
Alejandro Coca <alxmcr@outlook.com>
2024-10-31 00:07:54 -0400

    wip

--- src/components/containers/QuestionsContainer/index.tsx
+++ src/components/containers/QuestionsContainer/index.tsx
@@ -34,7 +34,7 @@ export function QuestionsContainer({
 
   return (
     <div className="questions-container">
-      <h2 className="questions-page__subtitle">
+      <h2 className="questions-container__subtitle">
         Questions {currentIndexQuestion + 1} / {questions.length}
       </h2>
       <QuestionContainer question={questions[currentIndexQuestion - 1]} />

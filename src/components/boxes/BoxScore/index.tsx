14cd991bee571ca861ba13fb9b83ce6f6b114c0d
Alejandro Coca <alxmcr@outlook.com>
2024-11-01 14:58:37 -0400

    wip

---
+++ src/components/boxes/BoxScore/index.tsx
@@ -0,0 +1,5 @@
+import "./BoxScore.scss";
+
+export function BoxScore() {
+  return <div className="box-score"></div>;
+}
--- src/pages/ScorePage/index.tsx
+++ src/pages/ScorePage/index.tsx
@@ -1,9 +1,12 @@
+import { BoxResults } from "../../components/boxes/BoxResults";
 import "./ScorePage.scss";
 
 export function ScorePage() {
   return (
     <main className="score-page">
-      <div className="score-page__container"></div>
+      <div className="score-page__container">
+        <BoxResults />
+      </div>
     </main>
   );
 }

62dbac0699e4f960137b7ed72403a7c3793dca52
Alejandro Coca <alxmcr@outlook.com>
2024-11-01 19:31:29 -0400

    wip

---
+++ src/types/app.enums.ts
@@ -0,0 +1,12 @@
+export enum LoadingStates {
+  IDLE = "IDLE",
+  PENDING = "PENDING",
+  SUCCESS = "SUCCESS",
+  ERROR = "ERROR",
+}
+
+export enum KindOfScore {
+  PERFECT = "perfect",
+  GREAT_WORK = "great_work",
+  FAILED = "failed",
+}

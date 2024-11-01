6f14c31b622e8d504a55b768d496508fdc71fbda
Alejandro Coca <alxmcr@outlook.com>
2024-11-01 17:31:06 -0400

    wip

--- src/components/boxes/BoxScore/index.tsx
+++ src/components/boxes/BoxScore/index.tsx
@@ -1,5 +1,18 @@
 import "./BoxScore.scss";
 
-export function BoxScore() {
-  return <div className="box-score"></div>;
+type Props = {
+  title: string;
+  score: number;
+  maxScore: number;
+};
+
+export function BoxScore({ title = "", score = 0, maxScore = 0 }: Props) {
+  return (
+    <div className="box-score">
+      <h2 className="box-score__title">{title}</h2>
+      <p className="box-score__score">
+        {score} / {maxScore}
+      </p>
+    </div>
+  );
 }

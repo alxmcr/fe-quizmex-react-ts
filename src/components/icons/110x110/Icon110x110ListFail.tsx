bf4ceb5ce80185252cd5725dd6f9d317a1caccc9
Alejandro Coca <alxmcr@outlook.com>
2024-11-01 20:29:44 -0400

    improvements

---
+++ src/components/icons/110x110/Icon110x110ListFail.tsx
@@ -0,0 +1,34 @@
+export function Icon110x110ListFail(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      width={110}
+      height={110}
+      viewBox="0 0 110 110"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        d="M47.6 27.6571H92M47.6 54.0571H92M47.6 80.4572H92M19.85 22L30.95 33.3143M30.95 22L19.85 33.3143"
+        stroke="currentColor"
+        strokeWidth={2.66667}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M25.4 61.6C29.4869 61.6 32.8 58.2229 32.8 54.0571C32.8 49.8913 29.4869 46.5143 25.4 46.5143C21.3131 46.5143 18 49.8913 18 54.0571C18 58.2229 21.3131 61.6 25.4 61.6Z"
+        stroke="currentColor"
+        strokeWidth={2.66667}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M25.4 88C29.4869 88 32.8 84.623 32.8 80.4572C32.8 76.2914 29.4869 72.9143 25.4 72.9143C21.3131 72.9143 18 76.2914 18 80.4572C18 84.623 21.3131 88 25.4 88Z"
+        stroke="currentColor"
+        strokeWidth={2.66667}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}

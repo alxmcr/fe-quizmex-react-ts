1c0fc020372d5a9a2c0a2150a942af5ead378786
Alejandro Coca <alxmcr@outlook.com>
2024-11-02 02:07:18 -0400

    wip

--- src/pages/ScorePage/index.tsx
+++ src/pages/ScorePage/index.tsx
@@ -1,14 +1,22 @@
+import React from "react";
 import { BoxResults } from "../../components/boxes/BoxResults";
 import { BoxScore } from "../../components/boxes/BoxScore";
-import { AppNavLink } from "../../components/links/AppNavLink";
+import { AppButton } from "../../components/buttons/AppButton";
 import { useScoreVariant } from "../../hooks/useScoreVariant";
+import { QuizContext } from "../../providers/QuizProvider/QuizContext";
 import "./ScorePage.scss";
+import { resetQuizAction } from "../../store/quiz/actions/quizActions";
+import { useNavigate } from "react-router-dom";
 
 export function ScorePage() {
-  const maxScoreQuiz = 10;
-  const correctScoreQuiz = 10;
-  const incorrectScoreQuiz = maxScoreQuiz - correctScoreQuiz;
-  const { score } = useScoreVariant(correctScoreQuiz, maxScoreQuiz);
+  const navigate = useNavigate();
+  const { state, dispatch } = React.useContext(QuizContext);
+  const { score } = useScoreVariant(state.score, state.quiz.questions.length);
+
+  const onClickPlayAgain = () => {
+    dispatch(resetQuizAction());
+    navigate("/questions");
+  };
 
   if (score === null) {
     return null;
@@ -21,19 +29,19 @@ export function ScorePage() {
         <div className="score-page__scores">
           <BoxScore
             title="Correct"
-            score={correctScoreQuiz}
-            maxScore={maxScoreQuiz}
+            score={state.score}
+            maxScore={state.quiz.questions.length}
             variant={score.code}
           />
           <BoxScore
             title="Incorrect"
-            score={incorrectScoreQuiz}
-            maxScore={maxScoreQuiz}
+            score={state.quiz.questions.length - state.score}
+            maxScore={state.quiz.questions.length}
           />
         </div>
-        <AppNavLink to="/questions" variant={score.code}>
-          {correctScoreQuiz < 5 ? "Try again" : "Play again"}
-        </AppNavLink>
+        <AppButton variant={score.code} onClick={onClickPlayAgain}>
+          {state.score < 5 ? "Try again" : "Play again"}
+        </AppButton>
       </div>
     </main>
   );

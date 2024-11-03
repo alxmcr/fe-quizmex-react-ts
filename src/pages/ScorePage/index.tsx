5481aa72d1c8b40198620cb1f7dcedac90a0323b
Alejandro Coca <alxmcr@outlook.com>
2024-11-02 20:04:33 -0400

    wip

--- src/pages/ScorePage/index.tsx
+++ src/pages/ScorePage/index.tsx
@@ -1,14 +1,14 @@
+import React from "react";
 import { BoxResults } from "../../components/boxes/BoxResults";
 import { BoxScore } from "../../components/boxes/BoxScore";
 import { AppButton } from "../../components/buttons/AppButton";
-import { useScoreVariant } from "../../hooks/useScoreVariant";
+import { useScoreByQuiz } from "../../hooks/useScoreByQuiz";
+import { QuizContext } from "../../providers/QuizProvider/QuizContext";
 import "./ScorePage.scss";
 
 export function ScorePage() {
-  const maxScoreQuiz = 10;
-  const correctScoreQuiz = 10;
-  const incorrectScoreQuiz = maxScoreQuiz - correctScoreQuiz;
-  const { score } = useScoreVariant(correctScoreQuiz, maxScoreQuiz);
+  const { state } = React.useContext(QuizContext);
+  const { score } = useScoreByQuiz(state.quiz, state.score);
 
   if (score === null) {
     return null;
@@ -21,18 +21,18 @@ export function ScorePage() {
         <div className="score-page__scores">
           <BoxScore
             title="Correct"
-            score={correctScoreQuiz}
-            maxScore={maxScoreQuiz}
+            score={state.score}
+            maxScore={state.quiz.questions.length}
             variant={score.code}
           />
           <BoxScore
             title="Incorrect"
-            score={incorrectScoreQuiz}
-            maxScore={maxScoreQuiz}
+            score={state.quiz.questions.length - state.score}
+            maxScore={state.quiz.questions.length}
           />
         </div>
         <AppButton variant={score.code}>
-          {correctScoreQuiz < 5 ? "Try again" : "Play again"}
+          {state.score < 5 ? "Try again" : "Play again"}
         </AppButton>
       </div>
     </main>
